bb11d5465d3175443834a4b4d3a95fb4b6c4d2d0
Bethsheba Z <bethzebata@gmail.com>
2020-06-12 14:55:27 -0500

    Attempting to commit changes from aster for screens index

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,8 +1,4 @@
 export * from "./Home";
 export * from "./Profile";
 export * from "./NotFound";
-<<<<<<< HEAD
-export * from "./ProfilePage";
-=======
 export * from "./MessagesScreen";
->>>>>>> b41ade0b09ca495dc75d301a7641021618584275
