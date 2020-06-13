a72481346ea6a54c940a0d11dec561025e2c614d
Bethsheba Z <bethzebata@gmail.com>
2020-06-13 18:59:48 -0500

    Added header to all pages

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Jumbotron } from "react-bootstrap";
 import { ProfileCardContainer } from "../components";
 
 export const LandingPageContainer = () => (
   <>
+    <Jumbotron fluid>
+      <h1>Everything Alien</h1>
+    </Jumbotron>
     <h1>Testing</h1>
     <h2>Profile</h2>
     <ProfileCardContainer />
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { ProfileCardContainer, ProfilePageContainer } from "../components";
+import { Jumbotron } from "react-bootstrap";
 
 export const ProfileScreen = () => (
   <>
+    <Jumbotron fluid>
+      <h1>Everything Alien</h1>
+    </Jumbotron>
     <h1>Testing2</h1>
     <h2>Profile</h2>
     <ProfileCardContainer />
